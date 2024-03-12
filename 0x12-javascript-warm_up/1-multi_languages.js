5ab4c1f5f7be4b08b3d6c7bbf9b46abcc6be07a9
calvin sewornu <calvinsewornu123@gmail.com>
2024-03-12 02:44:18 +0000

    completed normal tasks

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-let cprog = 'C is fun';
-let pyth = 'Python is cool';
-let javascrp = 'JavaScript is amazing';
-
-console.log(cprog);
-console.log(pyth);
-console.log(javascrp);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
 
 
 
